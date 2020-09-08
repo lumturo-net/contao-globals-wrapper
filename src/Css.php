032ec2dc430b4c99e9e9e54f76e51d0d10df66ae
Christian Wederka <christian.wederka@lupcom.de>
2020-09-08 11:42:24 +0200

    * GLOBALS['TL_CSS'] als Array initialisieren falls nicht vorhanden

--- src/Css.php
+++ src/Css.php
@@ -18,6 +18,8 @@ class Css
      */
     public static function push(array $css)
     {
+        is_array($GLOBALS['TL_CSS']) ? null : $GLOBALS['TL_CSS'] = [];
+
         foreach ($css as $item) {
             if (in_array($item, $GLOBALS['TL_CSS'])) {
                 throw new CssExistsException('The given CSS item "' . $item . '" already exists in TL_CSS');
